61e8df028ed583a5778b19385eff5acb631c8fd7
Neeraj tiwari <nt98924@gmail.com>
2023-03-15 20:58:37 +0530

    deleted something or added something

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Hello i am react App</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Neeraj tiwari
+          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
